7109ef1efe9d15097cf3b752a988312573f39a03
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-07-01 13:12:12 -0400

    update getPost

--- post-mean-stack/src/app/posts/Service/post.service.ts
+++ post-mean-stack/src/app/posts/Service/post.service.ts
@@ -47,7 +47,9 @@ export class PostService {
   }
 
   getPostByID(id: string) {
-    return { ...this.posts.find((post) => post.id === id) };
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
   }
 
   addPost(title: string, content: string) {
@@ -67,6 +69,19 @@ export class PostService {
       .subscribe(() => {
         console.log('Deleted');
         const updatedPost = this.posts.filter((post) => post.id !== postID);
+        this.posts = updatedPost;
+        this.postUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const updatedPost = [...this.posts];
+        const oldPostIndex = updatedPost.findIndex((p) => p.id === post.id);
+        updatedPost[oldPostIndex] = post;
         this.posts = updatedPost;
         this.postUpdated.next([...this.posts]);
       });
