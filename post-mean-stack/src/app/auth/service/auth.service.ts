b8c5e9c886d010ff65927d26d379d62d7221af7f
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-07-09 14:33:51 -0400

    spa authentication

---
+++ post-mean-stack/src/app/auth/service/auth.service.ts
@@ -0,0 +1,20 @@
+import { AuthData } from './../model/auth-data.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = {
+      email: email,
+      password: password,
+    };
+    return this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe((result) => {
+        console.log(result);
+      });
+  }
+}
